446f2a4f4dc0708b46318162e6603b24498fef23
Sammul Wu <wuxia1989@gmail.com>
2014-09-19 15:14:57 -0700

    919

--- KLowestCommonAncestor.java
+++ KLowestCommonAncestor.java
@@ -1,1 +1,26 @@
 //(Binary tree) K nodes’ lowest common ancestor
+public class KLowestCommonAncestor{
+	public Node LCA(Node root, Node a, Node b){
+		if(root==null)
+			return null;
+		if(root==a || root==b)
+			return root;
+		Node left = LCA(root.left,a,b);
+		Node right = LCA(root.right,a,b);
+		if(left!=null && right!=null)
+			return root;
+		else
+			return left==null? right : left;
+	}
+	public Node kLCA(Node root, ArrayList<Node> l){
+		
+	}
+}
+
+class Node{
+	Node left, right;
+	int val;
+	Node(int i){
+		this.val = i;
+	}
+}
